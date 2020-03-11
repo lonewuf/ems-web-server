f44f4c4803a8b2d97be27862ead8571ee1712bf4
tawbuts <john.nico.austria@gmail.com>
2020-03-12 04:32:08 +0800

    First commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb://localhost/oop-dev',
+  // database: 'mongodb://isabuhay:l0n3wolf@ds133086.mlab.com:33086/gulapagadgets-dev',
+  databaseProd:'mongodb://nico:l0n3wolf@ds133547.mlab.com:33547/vidjot-prod'
+}   
